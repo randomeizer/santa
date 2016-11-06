6169890e5de0c91c9b3a0a5a9984bfe87bd53050
David Peterson <david@randombits.org>
2016-11-06 10:17:23 +1000

    * Extracted the 'people' database to an external people.json file

--- santa.js
+++ santa.js
@@ -1,36 +1,35 @@
 var nodemailer = require("nodemailer");
+var jsonfile = require("jsonfile");
+
+var argv = require('minimist')(process.argv.slice(2));
 
 // create reusable transporter object using SMTP transport
 var transporter = nodemailer.createTransport({
-    service: 'Gmail',
-    auth: {
-        user: 'santa@petersonexpress.net',
-        pass: 'Rudolph2015'
-    }
+  service: 'Gmail',
+  auth: {
+    user: 'santa@petersonexpress.net',
+    pass: 'Rudolph2015'
+  }
 });
 
-var people = [
-  {first: "John", last: "Peterson", email: "john@petersonexpress.net", exclude: ["Glenda","David","Andrew"]},
-  {first: "Glenda", last: "Peterson", email: "glenda@petersonexpress.net", exclude: ["John","David","Andrew"]},
-  {first: "David", last: "Peterson", email: "david@randombits.org", exclude: ["Andrew","Monique"]},
-  {first: "Andrew", last: "Peterson", email: "andrew@petersonexpress.net", exclude: ["David","Loch"]},
-  {first: "Philip", last: "Peterson", email: "theprips@gmail.com", exclude: ["Monique","Glenda","Heidi"]},
-  {first: "Monique", last: "Peterson", email: "missmrw@gmail.com", exclude: ["Philip","Glenda","Heidi"]},
-  {first: "Heidi", last: "Oakley", email: "heidi@petersonexpress.net", exclude: ["Loch","John","Philip"]},
-  {first: "Loch", last: "Oakley", email: "lochoakley@hotmail.com", exclude: ["Heidi","Philip","John"]}
-]
+var people = jsonfile.readFileSync("people.json");
 
+/********* Start Application **********/
 var list = createList(people);
 mailList(list);
 console.log("Secret Santa List complete!");
+/*********  End Application  **********/
 
 function createList(people) {
   // Create the recipient list
   var recipients = createRecipients(people);
-  
+
   var list = [];
   for (var i = 0; i < people.length; i++) {
-    list[i] = {from: people[i], to: recipients[i]};
+    list[i] = {
+      from: people[i],
+      to: recipients[i]
+    };
   }
   return list;
 }
@@ -42,31 +41,34 @@ function createRecipients(people) {
   for (var i = 0; i < people.length; i++) {
     recipients[i] = people[i];
   }
-  
+
   do {
     shuffles++;
     shuffle(recipients);
   } while (badRecipients(people, recipients));
-  
+
   console.log("Created Recipient List after " + shuffles + " shuffles.");
   return recipients;
 }
 
 function badRecipients(people, recipients) {
   for (var i = 0; i < people.length; i++) {
-    var from = people[i]; var to = recipients[i];
-    if (from.first === to.first)
+    var from = people[i];
+    var to = recipients[i];
+    // If sending to ourself or our partner, it's bad
+    if (from.first === to.first || from.partner === to.first)
       return true;
-    if (from.exclude.indexOf(to.first) >= 0)
+    // If sending to someone we gave to in the last two years, it's bad
+    if (from.history.slice(-2).indexOf(to.first) >= 0)
       return true;
   }
   return false;
 }
 
-function shuffle (array) {
-  var i = 0
-    , j = 0
-    , temp = null
+function shuffle(array) {
+  var i = 0,
+    j = 0,
+    temp = null
 
   for (i = array.length - 1; i > 0; i -= 1) {
     j = Math.floor(Math.random() * (i + 1))
@@ -92,17 +94,17 @@ function sendEmail(giver, receiver) {
   // setup e-mail data with unicode symbols
   var mailOptions = {
     from: 'Secret Santa <santa@petersonexpress.net>', // sender address
-//    to: "David Peterson <david@randombits.org>",
+    //    to: "David Peterson <david@randombits.org>",
     to: giver.first + " " + giver.last + " <" + giver.email + ">", // list of receivers
     subject: 'Your Secret Santa is....', // Subject line
     text: 'Hi ' + giver.first + ",\n\nYour Secret Santa this year is " + receiver.first + ".\n\nMerry Christmas!", // plaintext body
   };
 
   // send mail with defined transport object
-  transporter.sendMail(mailOptions, function(error, info){
-      if(error){
-          return console.log(error);
-      }
-      console.log('Message sent to '+giver.first+': ' + info.response);
+  transporter.sendMail(mailOptions, function(error, info) {
+    if (error) {
+      return console.log(error);
+    }
+    console.log('Message sent to ' + giver.first + ': ' + info.response);
   });
 }
