a4dc92ff1ea7ea6563f2e8749f1a77101c566e1a
David Peterson <david@randombits.org>
2026-01-13 13:19:31 +1000

    Refactor data storage: replace crypto-fs with new .sack format and implement migration for legacy .dat files

--- santa.js
+++ santa.js
@@ -1,58 +1,14 @@
 require('string.prototype.endswith');
 var nodemailer = require("nodemailer");
 const fs = require('fs');
-const crypto = require('crypto');
+const sackStore = require("./lib/sack-store");
+const legacyStore = require("./lib/legacy-store");
 
-function patchLegacyCipher() {
-  if (typeof crypto.createCipher === "function" && typeof crypto.createDecipher === "function") {
-    return;
-  }
-
-  function getKeyIv(algorithm, password) {
-    var keyLen = 32;
-    var ivLen = 16;
-
-    if (algorithm.indexOf("aes-128-") === 0) {
-      keyLen = 16;
-    } else if (algorithm.indexOf("aes-192-") === 0) {
-      keyLen = 24;
-    }
-
-    var data = Buffer.isBuffer(password) ? password : Buffer.from(String(password), "utf8");
-    var derived = Buffer.alloc(0);
-    var prev = Buffer.alloc(0);
-
-    while (derived.length < keyLen + ivLen) {
-      var hash = crypto.createHash("md5");
-      hash.update(prev);
-      hash.update(data);
-      prev = hash.digest();
-      derived = Buffer.concat([derived, prev]);
-    }
-
-    return {
-      key: derived.slice(0, keyLen),
-      iv: derived.slice(keyLen, keyLen + ivLen)
-    };
-  }
-
-  crypto.createCipher = function(algorithm, password) {
-    var parts = getKeyIv(algorithm, password);
-    return crypto.createCipheriv(algorithm, parts.key, parts.iv);
-  };
-
-  crypto.createDecipher = function(algorithm, password) {
-    var parts = getKeyIv(algorithm, password);
-    return crypto.createDecipheriv(algorithm, parts.key, parts.iv);
-  };
-}
-
-patchLegacyCipher();
-
-var cfs = require('crypto-fs');
-
-const DEFAULT_DATA_FILE = "people.dat";
+const DEFAULT_DATA_FILE = "santa.sack";
+const LEGACY_DATA_FILE = "people.dat";
 const ENV_FILE = ".env";
+const SACK_EXT = ".sack";
+const LEGACY_EXT = ".dat";
 
 function loadEnvFile(path) {
   if (!fs.existsSync(path)) {
@@ -113,13 +69,6 @@ function getTransporter() {
   return transporter;
 }
 
-// Initialise crypto
-// Note: We're really just encrypting so that the person running it doesn't accidentally see the list...
-cfs.init({
-  password: "SecretSanta",
-  root: process.cwd()
-});
-
 /********* Start Application **********/
 function main() {
   if (argv.help) {
@@ -127,8 +76,19 @@ function main() {
     process.exit(0);
   }
 
+  if (argv.migrate) {
+    migrateLegacy();
+    return;
+  }
+
   var inFile = argv.in ? argv.in : DEFAULT_DATA_FILE;
   var outFile = argv.out ? argv.out : inFile;
+
+  if (!argv.in && !fs.existsSync(inFile) && legacyStore.existsSync(LEGACY_DATA_FILE)) {
+    console.error("Missing '" + inFile + "'. Legacy '" + LEGACY_DATA_FILE + "' detected.");
+    console.error("Run `node santa.js --migrate` to create a new .sack file.");
+    process.exit(10);
+  }
 
   // Load people
   var people = loadPeople(inFile);
@@ -151,14 +111,17 @@ function main() {
 function loadPeople(inFile) {
   var peopleText;
 
-  if (inFile.endsWith(".dat")) {
-    if (!cfs.existsSync(inFile)) {
+  if (inFile.endsWith(LEGACY_EXT)) {
+    console.error("Legacy data file detected. Run `node santa.js --migrate` first.");
+    process.exit(10);
+  } else if (inFile.endsWith(SACK_EXT)) {
+    if (!fs.existsSync(inFile)) {
       console.log("Unable to find '" + inFile + "'.");
       console.log("You can specify an alternate file to load by adding '--in=<path>'.");
       process.exit(10);
     }
     console.log("Decrypting '" + inFile + "'...");
-    peopleText = cfs.readFileSync(inFile);
+    peopleText = sackStore.readFileSync(inFile, requireSackPass());
     console.log("Decrypted.")
   } else if (inFile.endsWith(".json")) {
     if (!fs.existsSync(inFile)) {
@@ -339,8 +302,11 @@ function savePeople(people, outFile) {
   console.log("Saving people to '" + outFile + "'");
   var peopleText = JSON.stringify(people, null, 2);
 
-  if (outFile.endsWith(".dat")) {
-    cfs.writeFileSync(outFile, peopleText);
+  if (outFile.endsWith(LEGACY_EXT)) {
+    console.error("Refusing to write legacy data file. Use `node santa.js --migrate` instead.");
+    process.exit(2);
+  } else if (outFile.endsWith(SACK_EXT)) {
+    sackStore.writeFileSync(outFile, peopleText, requireSackPass());
   } else if (outFile.endsWith(".json")) {
     fs.writeFileSync(outFile, peopleText);
   } else {
@@ -349,32 +315,83 @@ function savePeople(people, outFile) {
   }
 }
 
+function migrateLegacy() {
+  var inFile = argv.in ? argv.in : LEGACY_DATA_FILE;
+  var outFile = argv.out ? argv.out : DEFAULT_DATA_FILE;
+
+  if (!inFile.endsWith(LEGACY_EXT)) {
+    console.error("Migration input must be a .dat file.");
+    process.exit(2);
+  }
+
+  if (!outFile.endsWith(SACK_EXT)) {
+    console.error("Migration output must be a .sack file.");
+    process.exit(2);
+  }
+
+  if (fs.existsSync(outFile)) {
+    console.error("Refusing to overwrite existing '" + outFile + "'.");
+    process.exit(2);
+  }
+
+  if (!legacyStore.existsSync(inFile)) {
+    console.error("Unable to find legacy file '" + inFile + "'.");
+    console.error("You can specify an alternate file to load by adding '--in=<path>'.");
+    process.exit(10);
+  }
+
+  console.log("Migrating legacy '" + inFile + "' to '" + outFile + "'...");
+  var peopleText = legacyStore.readFileSync(inFile);
+
+  try {
+    JSON.parse(peopleText);
+  } catch (err) {
+    console.error("Legacy file could not be parsed as JSON.");
+    process.exit(2);
+  }
+
+  sackStore.writeFileSync(outFile, peopleText, requireSackPass());
+  console.log("Migration complete.");
+}
+
+function requireSackPass() {
+  if (!process.env.SANTA_SACK_PASS) {
+    console.error("Missing SANTA_SACK_PASS. Set it before using .sack files.");
+    process.exit(2);
+  }
+  return process.env.SANTA_SACK_PASS;
+}
+
 function help() {
   console.log("This script generates a Secret Santa list for the Peterson family.");
-  console.log("By default, this will simply load the 'people.dat' file and then save it again.");
+  console.log("By default, this will load 'santa.sack' and then save it again.");
   console.log("");
   console.log("Options:");
-  console.log("\t--in=<path> - (default 'people.dat') Specify the input path. '*.dat' files will be encrypted, '*.json' will be plain text.");
-  console.log("\t--out=<path> - (default to the 'in' path) Specify the output path. '*.dat' will be encrypted, '*.json' will be plain text.");
+  console.log("\t--in=<path> - (default 'santa.sack') Specify the input path. '*.sack' files are encrypted, '*.json' are plain text.");
+  console.log("\t--out=<path> - (default to the 'in' path) Specify the output path. '*.sack' files are encrypted, '*.json' are plain text.");
   console.log("\t--generate - will generate a new list of gift givers for the current year.");
   console.log("\t--send - if specified, each giver will be sent their secret recipient in an email.");
   console.log("\t--test-email=<email address> - if an email address is provided, it will be used instead of the giver's email address.");
   console.log("\t--subject-prefix=<prefix> - the provided text will be prefixed to the standard subject line.")
   console.log("\t--print - If specified, the people data loaded will be printed.");
+  console.log("\t--migrate - migrate legacy .dat data to .sack format (supports --in/--out).");
   console.log("");
   console.log("Email settings:");
   console.log("\tSANTA_SMTP_USER - SMTP username (defaults to santa@petersonexpress.net)");
   console.log("\tSANTA_SMTP_PASS - SMTP password (required when using --send)");
+  console.log("\tSANTA_SACK_PASS - password for encrypting/decrypting .sack files");
   console.log("\t.env - if present, will be loaded for these variables");
   console.log("");
   console.log("To generate, send out and save a new set of Secret Santa emails, do this:");
   console.log("\tnode santa.js --generate --send");
   console.log("To decrypt the current data file, do this:")
   console.log("\tnode santa.js --out=my-data.json");
-  console.log("To encrypt a JSON file to 'people.dat', do this:");
-  console.log("\tnode santa.js --in=my-data.json --out=people.dat")
-  console.log("To print the contents of 'people.dat', do this:");
+  console.log("To encrypt a JSON file to 'santa.sack', do this:");
+  console.log("\tnode santa.js --in=my-data.json --out=santa.sack")
+  console.log("To print the contents of 'santa.sack', do this:");
   console.log("\tnode santa.js --print")
+  console.log("To migrate from legacy data, do this:");
+  console.log("\tnode santa.js --migrate")
 }
 
 if (require.main === module) {
